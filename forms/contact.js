3ce3796c843b8302ebb519da90c56e815e07578e
Víctor Andrés <102297884+vandresca@users.noreply.github.com>
2023-08-24 15:37:15 +0200

    Update and rename contact.php to contact.js

---
+++ forms/contact.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const smtp = require('nodemailer');
+
+const app = express();
+app.use(bodyParser.json());
+
+// Replace contact@example.com with your real receiving email address
+const receivingEmailAddress = 'contact@example.com';
+
+// Create a new SMTP client
+const transporter = smtp.createTransport({
+  host: 'smtp.example.com',
+  port: 587,
+  auth: {
+    user: 'example',
+    password: 'pass',
+  },
+});
+
+app.post('/contact', (req, res) => {
+  // Get the form data
+  const name = req.body.name;
+  const email = req.body.email;
+  const subject = req.body.subject;
+  const message = req.body.message;
+
+  // Create a new email message
+  const mail = {
+    from: {
+      name: name,
+      email: email,
+    },
+    to: receivingEmailAddress,
+    subject: subject,
+    text: message,
+  };
+
+  // Send the email message
+  transporter.sendMail(mail, (err) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send('Email sent successfully');
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
